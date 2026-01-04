30659fcbebb90ad585e232fcb76eb3d776f739d4
Javier Quiroga <contact@javierquiroga.com>
2026-01-04 15:34:27 +0000

    feat: Add fonts and global css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import './global.css';
+
 import routes from './routes';
 
 const router = createBrowserRouter(routes);
